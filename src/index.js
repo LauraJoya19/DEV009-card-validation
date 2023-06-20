260644e3d5a22fc700d7148ed4ff75897e72502b
LauraJoya19 <lalujoya@gmail.com>
2023-06-19 23:14:00 -0500

    proyecto finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import validator from './validator.js';
 
-console.log(validator);
+const modal = document.querySelector("#modal"); // declaramos variable modal 
+const myForm = document.querySelector("#form"); // declaramos variable myForm
+
+document.querySelector("#myBtn").addEventListener("click", () => modal.style.display = "block"); // se agrega un evento de escucha al elemento del dom para q muestre la pantalla modal
+document.querySelector(".close").addEventListener("click", () => { 
+  modal.style.display = "none"; // cierre de ventana modal
+  clearFormValues(); // funcion reiniciar el formulario
+});
+
+function clearFormValues() { // funcion para reiniciar el formulario
+  const elements = ["nroTarjeta", "donanteName", "codeVer", "validar", "maskedCard"]; //array con los ids de los elementos que se van a reiniciar
+  elements.forEach(element => document.getElementById(element).textContent = ""); // se establece el contenido en una cadena vacia para borrar el contenido existente
+  myForm.reset(); // reestablece el formulario a su valor predeterminado
+}
+
+document.getElementById("myDonation").addEventListener("click", (e) => { //funciones al dar click en el boton
+  e.preventDefault(); //evita q se envie el formulario al darle click
+
+  const [ //declaramos las variables hacemos un solo llamado al document.getElementById
+    creditCardNumber, nombreDonante, cvv, nroTarjeta, donanteName, codeVer, validar, maskedCard] = 
+    ["creditCardNumber", "nombreDonante", "cvv", "nroTarjeta", "donanteName", "codeVer", "validar", "maskedCard" ].map(id => document.getElementById(id)); //es un nuevo array que contiene los elementos seleccionados del DOM
+
+  [nroTarjeta, donanteName, codeVer].forEach(errorElement => errorElement.textContent = ""); // elimina los mensajes de error antes de reestablecer el formulario
+
+  if (creditCardNumber.value === "") { //si el valor del campo nro tarjeta esta vacio arroja mensaje 
+    nroTarjeta.textContent = "Introduce un número de tarjeta.";
+    return;
+  }
+
+  if (nombreDonante.value === "") { //si el valor del campo nombre esta vacio arroja mensaje
+    donanteName.textContent = "Introduce nombre.";
+    return;
+  }
+
+  if (cvv.value === "") { // si el valor del campo cvv esta vacio arroja mensaje
+    codeVer.textContent = "Introduce CVV.";
+    return;
+  }
+  // resultado, traemos la funcion del isvalid y maskify nos indica si es valida o invalida y nos muestra el numero enmascarado
+  const result = validator.isValid(creditCardNumber.value);
+  validar.innerHTML = result ? "Tarjeta Válida" : "Tarjeta Inválida";
+  maskedCard.innerHTML = validator.maskify(creditCardNumber.value);
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,47 @@
-const validator = {
-  // ...
+const validator = { //validator contiene dos funciones  
+  isValid, maskify,
 };
+function isValid (number) { //definicion de la funcion isValid
+  const reverseNumber = number.split("").map(Number).reverse(); //se crea un nuevo array con los numero invertidos
+  let sumaPar = 0; //variable suma numero posicion par inicia con 0
+  let sumaImpar = 0; //variable suma numero posicion impar inicia con 0
+  for (let i = 0; i < reverseNumber.length; i++) { //bucle for variable i inicia en 0 si i es menor q el array se incrementa el valor de 1 para continuar en cada elemento
+    if (i % 2 === 1) { //verificamos si es impar
+      const resultPares = reverseNumber[i] * 2; //crea la variable resultPares para guardar el resultado
+      if (resultPares >= 10) { //se verifica si es mayor o igual a 10
+        sumaPar += Math.floor(resultPares / 10) + (resultPares % 10); //si es mayor se realiza la suma del numero entero y del modulo 
+      }
+      else {
+        sumaPar += resultPares; //si es menor q 10 el resultado se suma directo a la variable
+      }
+    }
+    else {
+      sumaImpar += reverseNumber[i]; // si no se cumple la condicion anterior se suma el numero directamente en la variable
+    }
+  }
+  const sumaTotal = sumaImpar + sumaPar; //suma el resultado en las varibales 
+    
+  if (sumaTotal % 10 === 0) { // si el valor de la variable es divide entre 10 y no deja residuo 
+    return true; // es verdadera
+  }
+  else {
+    return false; //sino es falsa
+  }
+}
+    
+function maskify (number) { //funcion enmascarar numero
+  let numeroEnmascarado = ""; //crea una cadena vacia
+  const cadena = number.length; // declara la variable cadena contiene la longitud del numero de la tarjeta
+  for (let i = 0; i < number.length; i++) { //bucle for itera sobre los numeros ingresados
+    if (i>(cadena-5)) { // si la cadena el indice i es mayor a la longitud del numero menos 5 se concatena al valor actual
+      numeroEnmascarado = numeroEnmascarado + number[i];
+    }
+    else {
+      numeroEnmascarado = numeroEnmascarado + "#"; // los demas numeros se concatenan en #
+    }
+  }
+  return numeroEnmascarado; //nos devuelve el numero enmascarado
+  
+} 
 
 export default validator;
